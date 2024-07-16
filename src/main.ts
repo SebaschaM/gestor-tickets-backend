359fb5b7177a612971025cb529d29eda4ea496f3
Sebastian <schaquila@autonoma.edu.pe>
2024-07-16 01:17:02 -0500

    chore: Update CORS origin to use production URL

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions = {
-    origin: "https://gestor-tickets.devioz.com",
+    origin: "*",
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     optionsSuccessStatus: 200,
